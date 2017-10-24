28c9f709b130e3ce6f15a1754a0dc2f71819123c
Guy Bachkhaznadji <guysergebach@hotmail.com>
2017-10-24 22:48:59 +0100

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 AdminUser.delete_all
 User.delete_all
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
 
 
